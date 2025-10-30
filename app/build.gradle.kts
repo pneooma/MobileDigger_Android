6e19c5ad502635ac2d56c1d44e8d7f7954470e31
Florescu Alin <alinflorinflorescu@gmail.com>
2025-10-31 01:35:31 +0200

    Update version to 10.30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.mobiledigger"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1023
-        versionName = "10.23"
+        versionCode = 1030
+        versionName = "10.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
